8b26400620ff175bd7b8bd45845efdff2218d5c3
PedroPereiraN <peapn.13@gmail.com>
2025-05-04 02:52:09 -0400

    refactor: custom main carousel

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
-import { ref, type Ref } from "vue";
+import { ref, TransitionGroup, type Ref } from "vue";
 import Product from "~/components/Product.vue";
 import { getCarouselImages } from "~/services/home-queries";
 import { getBestSellers, getProducts } from "~/services/product-queries";
@@ -12,11 +11,12 @@ import { numberMask } from "~/utils/masks";
 
 const carouselImages: HomeImageCarousel[] = await getCarouselImages();
 const bestSellers: ProductType[] = await getBestSellers();
-
 const quantityOfProducts: Ref<number> = ref(16);
 const products: Ref<ProductType[]> = ref(
   await getProducts(quantityOfProducts.value),
 );
+const carouselDirection: Ref<"left" | "right"> = ref("left");
+const showingImage: Ref<number> = ref(1);
 
 const showMoreProducts = async (): Promise<void> => {
   quantityOfProducts.value += 8;
@@ -24,52 +24,58 @@ const showMoreProducts = async (): Promise<void> => {
   products.value = await getProducts(quantityOfProducts.value);
 };
 
-const carouselConfig = {
-  itemsToShow: 1,
-  wrapAround: true,
-  autoplay: 5000,
-};
-
-const bestSellersCarouselConfig = {
-  itemsToShow: 4,
-  wrapAround: true,
-  gap: 20,
-};
+function changeImage(direction: "left" | "right") {
+  carouselDirection.value = direction;
+  if (direction == "left" && showingImage.value > 0) {
+    showingImage.value -= 1;
+  } else if (direction == "left" && showingImage.value == 0) {
+    showingImage.value = carouselImages.length - 1;
+  } else if (
+    direction == "right" &&
+    showingImage.value < carouselImages.length - 1
+  ) {
+    showingImage.value += 1;
+  } else if (
+    direction == "right" &&
+    showingImage.value == carouselImages.length - 1
+  ) {
+    showingImage.value = 0;
+  }
+}
 </script>
 
 <template>
   <main>
-    <Carousel v-bind="carouselConfig">
-      <Slide v-for="slide in carouselImages" :key="slide.id">
-        <img :src="slide.imageURL" />
-      </Slide>
-
-      <template #addons>
-        <div class="hidden md:block">
-          <Navigation />
-        </div>
-        <Pagination />
-      </template>
-    </Carousel>
-    <section class="m-20">
-      <h2 class="font-bold text-3xl mb-10">Best sellers</h2>
-
-      <Carousel v-bind="bestSellersCarouselConfig">
-        <Slide v-for="bestSeller in bestSellers" :key="bestSeller.id">
-          <Product
-            :item="{
-              image: bestSeller.imageURL,
-              name: bestSeller.name,
-              path: VIEW_PRODUCT(bestSeller.id),
-              price: numberMask(bestSeller.price, 2),
-            }"
-          />
-        </Slide>
-
-        <template #addons>
-          <Navigation class="-mx-16" />
+    <div class="">
+      <div class="relative w-screen h-screen">
+        <template v-for="(image, index) in carouselImages" :key="index">
+          <Transition :name="carouselDirection">
+            <img
+              :src="image.imageURL"
+              v-if="showingImage == index"
+              alt="Carousel item"
+              class="w-full h-full"
+            />
+          </Transition>
         </template>
-      </Carousel>
+        <button
+          type="button"
+          class="absolute left-6 top-[50%] text-3xl cursor-pointer"
+          @click="changeImage('left')"
+        >
+          <Icon name="lucide:chevron-left" />
+        </button>
+        <button
+          type="button"
+          class="absolute right-6 top-[50%] text-3xl cursor-pointer"
+          @click="changeImage('right')"
+        >
+          <Icon name="lucide:chevron-right" />
+        </button>
+      </div>
+    </div>
+    <section class="mb-10 md:m-20">
+      <h2 class="font-bold text-3xl m-10 md:mb-10">Best sellers</h2>
     </section>
 
     <section class="m-20">
@@ -105,5 +111,29 @@ const bestSellersCarouselConfig = {
   --vc-pgn-width: 12px;
   --vc-pgn-height: 12px;
   --vc-pgn-border-radius: 6px;
+}
+
+.right-enter-active,
+.left-enter-active,
+.right-leave-active,
+.left-leave-active {
+  position: absolute;
+  transition: all 0.3s ease-in-out;
+}
+
+.left-enter-from {
+  transform: translateX(-100%);
+}
+
+.left-leave-to {
+  transform: translateX(100%);
+}
+
+.right-enter-from {
+  transform: translateX(100%);
+}
+
+.right-leave-to {
+  transform: translateX(-100%);
 }
 </style>
