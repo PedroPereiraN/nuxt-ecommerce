c5e076f7096f95973914d41bf417dbc79a1d3ab0
PedroPereiraN <peapn.13@gmail.com>
2025-05-11 01:02:13 -0400

    feat: products carousel is now endless

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -25,22 +25,80 @@ const showMoreProducts = async (): Promise<void> => {
 
 const carouselDirection: Ref<"list-left" | "list-right"> = ref("list-left");
 const showingBestSellers: Ref<ProductType[]> = ref([]);
-showingBestSellers.value.push(bestSellers[0]);
+const showingFirstIndex: Ref<number> = ref(0);
+const showingLastIndex: Ref<number> = ref(2);
+showingBestSellers.value.push(bestSellers[showingFirstIndex.value]);
 showingBestSellers.value.push(bestSellers[1]);
-showingBestSellers.value.push(bestSellers[2]);
+showingBestSellers.value.push(bestSellers[showingLastIndex.value]);
 const canChangeImage: Ref<boolean> = ref(true);
+
+const getIndexOfFirstShowingItem = (): number | null => {
+  const firstCarouselItem = bestSellers.find(
+    (bs: { id: string }) => bs.id == showingBestSellers.value[0]?.id,
+  );
+
+  if (firstCarouselItem) {
+    return bestSellers.indexOf(firstCarouselItem);
+  }
+
+  return null;
+};
+
+const getIndexOfLastShowingItem = (): number | null => {
+  const lastCarouselItem = bestSellers.find(
+    (bs: { id: string }) => bs.id == showingBestSellers.value[2]?.id,
+  );
+
+  if (lastCarouselItem) {
+    return bestSellers.indexOf(lastCarouselItem);
+  }
+
+  return null;
+};
 
 function changeProductsList(direction: "left" | "right") {
   if (canChangeImage.value) {
     carouselDirection.value = `list-${direction}`;
+
+    if (
+      direction == "left" &&
+      showingBestSellers.value[0]?.id == bestSellers[0]?.id
+    ) {
+      showingFirstIndex.value = bestSellers.length - 1;
+    } else if (direction == "left") {
+      const firstIndex = getIndexOfFirstShowingItem();
+
+      if (firstIndex) {
+        showingFirstIndex.value = firstIndex - 1;
+      } else {
+        showingFirstIndex.value -= 1;
+      }
+    }
+
+    if (
+      direction == "right" &&
+      showingBestSellers.value[showingBestSellers.value.length - 1]?.id ==
+        bestSellers[bestSellers.length - 1]?.id
+    ) {
+      showingLastIndex.value = 0;
+    } else if (direction == "right") {
+      const lastIndex = getIndexOfLastShowingItem();
+
+      if (lastIndex) {
+        showingLastIndex.value = lastIndex + 1;
+      } else {
+        showingLastIndex.value += 1;
+      }
+    }
+
     if (direction == "left") {
       showingBestSellers.value.splice(showingBestSellers.value.length - 1, 1);
-      showingBestSellers.value.unshift(bestSellers[0]);
+      showingBestSellers.value.unshift(bestSellers[showingFirstIndex.value]);
     }
 
     if (direction == "right") {
       showingBestSellers.value.splice(0, 1);
-      showingBestSellers.value.push(bestSellers[bestSellers.length - 1]);
+      showingBestSellers.value.push(bestSellers[showingLastIndex.value]);
     }
 
     canChangeImage.value = false;
@@ -91,6 +149,8 @@ function changeProductsList(direction: "left" | "right") {
         </button>
       </div>
     </section>
+    {{ showingFirstIndex }}
+    {{ showingLastIndex }}
     <section class="m-20">
       <h2 class="font-bold text-3xl mb-10">For you</h2>
       <div class="grid grid-cols-4 gap-5">
@@ -124,13 +184,13 @@ function changeProductsList(direction: "left" | "right") {
 .list-left-move,
 .list-right-enter-active,
 .list-left-enter-active {
-  transition: all 1s ease-in-out;
+  transition: all 0.3s ease-in-out;
   position: static;
 }
 
 .list-right-leave-active,
 .list-left-leave-active {
-  transition: all 1s ease-in-out;
+  transition: all 0.3s ease-in-out;
   position: absolute;
 }
 
